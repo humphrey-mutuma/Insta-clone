2206b0609a453b58b45c42223d466c0f67482852
humphrey-mutuma <humphreymutuma01@gmail.com>
2020-12-23 21:03:45 +0300

    Register models to admin

--- IG/admin.py
+++ IG/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Posts)
+admin.site.register(Following)
+admin.site.register(Comments)
