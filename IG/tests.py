f1d3a931cd376a650a4746d0067ad8f1cbe0e2ff
humphrey-mutuma <humphreymutuma01@gmail.com>
2020-12-23 21:24:02 +0300

    create tests

--- IG/tests.py
+++ IG/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Profile,Posts,Comments,Following
 
-# Create your tests here.
+# Testing the 'Following' and 'Comments'
+
+class FollowingTestClass(TestCase):
+    def setUp(self):
+        self.esther=Following(username='mykey',followed='marabel')
+                            
+    def test_instance(self):
+        self.assertTrue(isinstance(self.mykey,Following))
+
+class CommentTestClass(TestCase):
+    def setUp(self):
+        self.first=Comment(post=1,
+                            username='mykey',
+                            comment='What a great throw-back',
+                            date='Jan 07, 2020, 08:50 a.m.',
+                            count=0)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.first,Comment))
